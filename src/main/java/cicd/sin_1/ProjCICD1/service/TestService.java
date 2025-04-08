75214dfac3e8d59e2bdd0c4c8299ccc8bcce202b
KJH0476 <148829100+KJH0476@users.noreply.github.com>
2025-04-08 21:52:13 +0900

    Update TestService.java

--- src/main/java/cicd/sin_1/ProjCICD1/service/TestService.java
+++ src/main/java/cicd/sin_1/ProjCICD1/service/TestService.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public class TestService {
 
     public String test() {
-        return "Hello World!, CICD 테스트 3";
+        return "Hello World!, CICD 테스트 4";
     }
 }
